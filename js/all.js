a02d3d09b74afba7b4b78c9df462ef8175086287
joanne <s6122712@gmail.com>
2019-06-30 21:15:02 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    
+    // ====== 手機版 menu ======
+    $('.js-menuBar').click(function(){
+        $('.l-header nav').toggleClass('js-menuShow');
+    });
+    
+});
